d34291b5f8054f78b9f10748f9df89c134d39c13
negrita79 <elisa.79@live.cl>
2017-11-06 19:18:02 -0300

    encabezado multilinea del programa cuenta

---
+++ src/pruebajava2/ProgramaCuenta.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava2;
+import javax.swing.JOptionPane;
+/************************************************************************
+ * Elda Solar
+ * @author LAB05
+ * Fecha: 06/11/2017
+ * ClasePrograma cuenta
+ * Responsabilidad:programa que pide contraseñas y sie pueden bloquear por 3 intentos fallidos
+ */
+public class ProgramaCuenta 
+{
+ 
+ public static void main(String[] args)
+ {
+
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ 
+ 
+     
+ } 
+    
+}
