3421c5930401f98980dddd9e24d101848764b5aa
negrita79 <elisa.79@live.cl>
2017-11-06 18:33:43 -0300

    , creado mes de diciembre con numero 12

--- src/pruebajava2/PruebaJava2.java
+++ src/pruebajava2/PruebaJava2.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package pruebajava2;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author LAB05
@@ -14,8 +14,16 @@ public class PruebaJava2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+         // TODO code application logic here
+    int año =Integer.parseInt(JOptionPane.showInputDialog("Ingrese numero"));
+   switch(año)
+           {
+        case 12:
+           JOptionPane.showMessageDialog(null, "diciembre");
+         break;
+         
     }
     
 }
